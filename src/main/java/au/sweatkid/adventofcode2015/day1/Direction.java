91e1284c205f5712e98c6014cee2d568a78251d7
PARAM <parammehta06@icloud.com>
2023-08-01 21:51:26 +0530

    Initial commit

---
+++ src/main/java/au/sweatkid/adventofcode2015/day1/Direction.java
@@ -0,0 +1,13 @@
+package au.sweatkid.adventofcode2015.day1;
+
+/**
+ * Advent of Code 2015 – Object-Oriented Solutions in Java.
+ *
+ * <p>The direction Santa can go (up / down).
+ *
+ * @author <a href="mailto:sven@happycoders.eu">Sven Woltmann</a>
+ */
+enum Direction {
+  UP,
+  DOWN
+}
---
+++ src/main/java/au/sweatkid/adventofcode2015/day10/Day10Main.java
@@ -0,0 +1,18 @@
+package eu.happycoders.adventofcode2015.day10;
+
+/**
+ * Advent of Code 2015 – Object-Oriented Solutions in Java.
+ *
+ * <p>Main application for day 10.
+ *
+ * @author <a href="mailto:sven@happycoders.eu">Sven Woltmann</a>
+ */
+class Day10Main {
+
+  private static final String INPUT = "3113322113";
+
+  public static void main(String[] args) {
+    System.out.println(Day10Solver.solvePart1(INPUT));
+    System.out.println(Day10Solver.solvePart2(INPUT));
+  }
+}
